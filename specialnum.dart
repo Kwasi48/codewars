46b88da6a2ac28520dca8f9df5d73adebe5346f2
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-09-19 11:51:13 +0000

    second funct

--- specialnum.dart
+++ specialnum.dart
@@ -15,5 +15,20 @@ void main() {
     return 'NOT!!';
   }
 
-  print(specialNumber(92));
+  String SpecialNum(n) {
+    List special = [1, 2, 3, 4, 5];
+    String num = n.toString();
+    List<String> numList = num.split('');
+    List<int> numtoInt = numList.map(int.parse).toList();
+    for (int spec in special) {
+      for (int nu in numtoInt) {
+        if (nu != spec || !special.contains(numtoInt)) {
+          return 'NOT!!';
+        }
+      }
+    }
+    return "Special!";
+  }
+
+  print(SpecialNum(11));
 }
