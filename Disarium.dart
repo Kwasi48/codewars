018426129775f85c9626fe8182073673b73a4181
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-02-01 15:34:05 +0000

    commented

--- Disarium.dart
+++ Disarium.dart
@@ -3,8 +3,8 @@ import 'dart:math';
 String disariumNumber(n) {
   String sN = n.toString();
   int sum = 0;
-  for (int i = 0; i < sN.length; ++i) {
-    sum += pow(int.parse(sN[i]), i + 1);
-  }
+  // for (int i = 0; i < sN.length; ++i) {
+  //   sum += pow(int.parse(sN[i]), i + 1);
+  // }
   return (sum == n) ? "Disarium !!" : "Not !!";
 }
