a25f346df857f9be2cfe8b047ccf376cb42b233f
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-16 11:28:42 +0000

    solving codewar of the day

---
+++ tidy.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  bool tidy(int n) {
+    if (n < 0) {
+      return false;
+    } else {
+      int counter = 0;
+      List numberToString = [];
+      numberToString.add(n);
+      while (counter < numberToString.length) {}
+      return true;
+    }
+  }
+}
