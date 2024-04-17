8877a49ddf6321875a280a5401c5e4803a95a2ad
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-17 11:25:57 +0000

    new challemge

--- tidy.dart
+++ tidy.dart
@@ -1,12 +1,22 @@
 void main(List<String> args) {
   bool? tidy(int n) {
+    if (n < 0) {
+      return false;
+    }
     //convert number to StringList.
     String charNum = n.toString();
     List<String> charList = charNum.split('');
     List<int> numList = charList.map(int.parse).toList();
-    print(charNum);
-    print(charList);
-    return true;
+    List<int> finalNum = [];
+    int number = 0;
+
+    // print(charList);
+    // print(numList);
+    if (numList == finalNum) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   print(tidy(34));
