5af6af03e663fff9b3431c712405da8e9236f0af
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-18 12:05:09 +0000

    unlocked solution, studying it

--- word_values.dart
+++ word_values.dart
@@ -1,3 +1,16 @@
 void main(List<String> args) {
   ///Word value problem.
+
+  List<int> wordValue(List<String> arr) {
+    List<int> ret = [];
+    int index = 0;
+    arr.forEach((String str) {
+      str.toLowerCase();
+      int num = 0;
+      str.runes.forEach(
+          (int char) => num += (char >= 97 && char <= 122) ? char - 96 : 0);
+      ret.add(num * ++index);
+    });
+    return (ret);
+  }
 }
