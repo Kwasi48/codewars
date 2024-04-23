82d51f5229911976cfaad7403741869f3dae335c
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-23 16:05:48 +0000

    another kaya XO started.

---
+++ exes_oh.dart
@@ -0,0 +1,8 @@
+//check to see if a string has the same amount of "x's" and "o's".
+//the method must return a boolean and can be case insensitive.
+//the String cann contain any char
+void main() {
+  bool XO(str) {
+    return true;
+  }
+}
