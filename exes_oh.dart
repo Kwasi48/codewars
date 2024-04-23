41e2736b982d46de8ba1a887a465e246fd3b1a17
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-23 16:40:52 +0000

    solved the kata ✨😎😁

--- exes_oh.dart
+++ exes_oh.dart
@@ -3,6 +3,44 @@
 //the String cann contain any char
 void main() {
   bool XO(str) {
-    return true;
+    //change the userInput to lowercase
+    String lower = str.toString().toLowerCase();
+    //split the String into a list
+    //also add  a counter
+    List<String> striz = lower.split('');
+    int counter1 = 0;
+    int counter2 = 0;
+    List<String> Xes = [];
+    List<String> Oes = [];
+    //loop through the list and add an element which is the same as 'x'
+    //to Xes List
+    // do the same for Oes list 'o'
+    while (counter1 < striz.length) {
+      for (var element in striz) {
+        if (element == 'x') {
+          Xes.add(element);
+        }
+      }
+      counter1++;
+    }
+//compare the lenght of both list at the end.
+    while (counter2 < striz.length) {
+      for (var element in striz) {
+        if (element == 'o') {
+          Oes.add(element);
+        }
+      }
+      counter2++;
+    }
+
+    if (Xes.length == Oes.length) {
+      return true;
+    } else {
+      return false;
+    }
+
+    //print(striz);
   }
+
+  print(XO('vix'));
 }
