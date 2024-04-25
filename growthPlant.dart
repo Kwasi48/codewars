a1404dc957d578429ab34592d6b8a8f1e77fde45
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-25 15:08:41 +0000

    some few bugs

--- growthPlant.dart
+++ growthPlant.dart
@@ -4,27 +4,31 @@
 // We plant the seed at the beginning of a day.
 // We want to know when the height of the plant will reach a certain level.
 
-int growingPlant(int upSpeed, int downSpeed, int desiredHeight) {
-  int plantHeight = 0;
-  int numberOfDays = 0;
-  if (upSpeed < 5 || upSpeed > 100) {
-    print("Please enter a number between 4 and 101");
-    return numberOfDays;
-  }
-  if (downSpeed < 2 || downSpeed > upSpeed) {
-    print("Please enter a number greater than 2 and less than upSeed");
-    return numberOfDays;
-  }
-  if (desiredHeight < 4 || desiredHeight > 1000) {
-    print("Please enter a number bigger 4 and less than 1001");
+void main() {
+  int growingPlant(int upSpeed, int downSpeed, int desiredHeight) {
+    int plantHeight = 0;
+    int numberOfDays = 0;
+    if (upSpeed < 5 || upSpeed > 100) {
+      print("Please enter a number between 4 and 101");
+      return numberOfDays;
+    }
+    if (downSpeed < 2 || downSpeed > upSpeed) {
+      print("Please enter a number greater than 2 and less than upSeed");
+      return numberOfDays;
+    }
+    if (desiredHeight < 4 || desiredHeight > 1000) {
+      print("Please enter a number bigger 4 and less than 1001");
+      return numberOfDays;
+    }
+
+    while (plantHeight < desiredHeight) {
+      plantHeight = plantHeight + upSpeed;
+      plantHeight = plantHeight - downSpeed;
+      numberOfDays++;
+    }
+
     return numberOfDays;
   }
 
-  while (plantHeight < desiredHeight) {
-    plantHeight = plantHeight + upSpeed;
-    plantHeight = plantHeight - downSpeed;
-    numberOfDays++;
-  }
-
-  return numberOfDays;
+  print(growingPlant(100, 10, 910));
 }
