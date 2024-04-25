cc592ad182071bddf0ada0555d537084824f4df9
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-25 14:47:14 +0000

    constraints set

--- growthPlant.dart
+++ growthPlant.dart
@@ -5,7 +5,18 @@
 // We want to know when the height of the plant will reach a certain level.
 
 int growingPlant(int upSpeed, int downSpeed, int desiredHeight) {
-  // your code here
+  if (upSpeed < 5 || upSpeed > 100) {
+    print("Please enter a number between 4 and 101");
+    return 0;
+  }
+  if (downSpeed < 2 || downSpeed > upSpeed) {
+    print("Please enter a number greater than 2 and less than upSeed");
+    return 0;
+  }
+  if (desiredHeight < 4 || desiredHeight > 1000) {
+    print("Please enter a number bigger 4 and less than 1001");
+    return 0;
+  }
 
   return 9;
 }
