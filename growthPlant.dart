f2d8d2e56799c23f458de493e9875132ef8487f1
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-04-25 15:03:37 +0000

    added some logic

--- growthPlant.dart
+++ growthPlant.dart
@@ -5,18 +5,26 @@
 // We want to know when the height of the plant will reach a certain level.
 
 int growingPlant(int upSpeed, int downSpeed, int desiredHeight) {
+  int plantHeight = 0;
+  int numberOfDays = 0;
   if (upSpeed < 5 || upSpeed > 100) {
     print("Please enter a number between 4 and 101");
-    return 0;
+    return numberOfDays;
   }
   if (downSpeed < 2 || downSpeed > upSpeed) {
     print("Please enter a number greater than 2 and less than upSeed");
-    return 0;
+    return numberOfDays;
   }
   if (desiredHeight < 4 || desiredHeight > 1000) {
     print("Please enter a number bigger 4 and less than 1001");
-    return 0;
+    return numberOfDays;
   }
 
-  return 9;
+  while (plantHeight < desiredHeight) {
+    plantHeight = plantHeight + upSpeed;
+    plantHeight = plantHeight - downSpeed;
+    numberOfDays++;
+  }
+
+  return numberOfDays;
 }
